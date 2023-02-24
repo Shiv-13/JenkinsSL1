82531f335770fe48d173711cd9ef48a84be8e9d4
Shiv-13 <56431311+Shiv-13@users.noreply.github.com>
2023-02-24 21:06:18 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
 	public static void main(String[] args){
 	//Prints A basic statement
 	System.out.println("Hello World");
-	System.out.println("Updated this line");	
+	System.out.println("Updated this line");
+	System.out.println("Updated this 2 line");
 	}
 }
